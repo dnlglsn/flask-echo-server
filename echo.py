06ec85f0a6ebd8149c0c95863d1ce9bf6609b3da
Daniel Gleason <danielg@surveymonkey.com>
2021-01-03 16:26:14 -0800

    My changes

--- echo.py
+++ echo.py
@@ -9,24 +9,29 @@ VERBOSE = 'verbose'
 BASIC_AUTH = 'basic_auth'
 AUTH_USERNAME = 'auth_username'
 AUTH_PASSWORD = 'auth_password'
+WAIT = 'wait'
 
 config = {
     BASIC_AUTH: False,
-    VERBOSE: False
+    VERBOSE: False,
+    WAIT: 0,
 }
 
 app = Flask(__name__)
 
-app.url_map.add(Rule('/', defaults={'path' : ''}, endpoint='index'))
+app.url_map.add(Rule('/', defaults={'path': ''}, endpoint='index'))
 app.url_map.add(Rule('/<path:path>', endpoint='index'))
+
 
 def validate_status_code(status_code):
     if status_code < 600:
         return True
     return False
 
+
 def extract(d):
-    return {key: value for (key, value) in d.items()}
+    return {key.lower(): value.lower() for (key, value) in d.items()}
+
 
 def check_auth(username, password):
     if AUTH_USERNAME not in config or AUTH_PASSWORD not in config:
@@ -34,11 +39,13 @@ def check_auth(username, password):
 
     return username == config[AUTH_USERNAME] and password == config[AUTH_PASSWORD]
 
+
 def authenticate():
     return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def requires_auth(f):
     @wraps(f)
@@ -51,6 +58,7 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
+
 @app.endpoint('index')
 @requires_auth
 def echo(path):
@@ -61,22 +69,22 @@ def echo(path):
         status_code = 200
 
     data = {
-        'success' : True,
-        'status' : status_code,
-        'time' : time.time(),
-        'path' : request.path,
-        'script_root' : request.script_root,
-        'url' : request.url,
-        'base_url' : request.base_url,
-        'url_root' : request.url_root,
-        'method' : request.method,
-        'headers' : extract(request.headers),
-        'data' : request.data.decode(encoding='UTF-8'),
-        'host' : request.host,
-        'args' : extract(request.args),
-        'form' : extract(request.form),
-        'json' : request.json,
-        'cookies' : extract(request.cookies)
+        'success': True,
+        'status': status_code,
+        'time': time.time(),
+        'path': request.path,
+        'script_root': request.script_root,
+        'url': request.url,
+        'base_url': request.base_url,
+        'url_root': request.url_root,
+        'method': request.method,
+        'headers': extract(request.headers),
+        'data': request.data.decode(encoding='UTF-8'),
+        'host': request.host,
+        'args': extract(request.args),
+        'form': extract(request.form),
+        'json': request.json,
+        'cookies': extract(request.cookies)
     }
 
     if config[VERBOSE]:
@@ -84,21 +92,33 @@ def echo(path):
 
     response = jsonify(data)
     response.status_code = status_code
+
+    # Modify response headers
+    response.headers = {'sm-feature-set-ids': 'set from origin server'}
+
+    if config[WAIT] > 0:
+        print('Waiting for %i seconds' % config[WAIT])
+        time.sleep(config[WAIT])
+
     return response
+
 
 def main():
     parser = OptionParser()
     parser.add_option('--port', dest='port', default=5000, help='port to run server on - default 5000')
     parser.add_option('--host', dest='host', default='127.0.0.1', help='host to bind server on - default 127.0.0.1')
-    parser.add_option('--auth', dest='auth', help='basic authentication credentials, should be passed in like "username:password"')
-    parser.add_option('-v', '--verbose', dest='verbose',
-        default=False, action='store_true', help='increased verbosity - outputs response to console')
+    parser.add_option('--auth', dest='auth',
+                      help='basic authentication credentials, should be passed in like "username:password"')
+    parser.add_option('--verbose', dest='verbose',
+                      default=False, action='store_true', help='increased verbosity - outputs response to console')
+    parser.add_option('--wait', dest='wait', default=0, help='how long to wait before sending a response', type=int)
     parser.add_option('--debug', dest='debug',
-        default=False, action='store_true', help='enable debug mode in flask')
+                      default=False, action='store_true', help='enable debug mode in flask')
 
     (options, args) = parser.parse_args()
 
     config[VERBOSE] = options.verbose
+    config[WAIT] = options.wait
 
     if options.auth:
         username, password = options.auth.split(':')
@@ -111,6 +131,7 @@ def main():
 
     app.debug = options.debug
     app.run(port=int(options.port), host=options.host)
+
 
 if __name__ == '__main__':
     main()
